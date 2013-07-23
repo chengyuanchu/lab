dc27e44bc88155b257ad5af66411d921acea091a
Anderson Chu <jengyuan0428@gmail.com>
2013-07-23 22:52:21 +0800

    test project

---
+++ test/src/tw/sample/first/hello.java
@@ -0,0 +1,13 @@
+package tw.sample.first;
+
+public class hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Good day! "+args[0]+".");
+	}
+
+}
